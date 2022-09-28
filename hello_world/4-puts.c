f553d36508b9706af588ae0e6bb8f4176abdbcb8
brncd <brunocarnales@gmail.com>
2022-09-28 10:13:38 -0700

    4-puts.c

---
+++ hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include <stdio.sh>
+/**
+ * main- Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int puts("with proper grammar, but the outcome is a piece of art,");
+	return (0);
+}
