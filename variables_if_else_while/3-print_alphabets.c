70810c4abc6a09217cff55f1028bb98fb11032e8
brncd <brunocarnales@gmail.com>
2022-09-29 11:46:13 -0700

    2-print_alphabet.c

---
+++ variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+/**
+* main- Entry point
+*
+* Return: Always 0 (Success)
+*/
+int main(void)
+{
+        char alphabet = 'a';
+	char Alphabet = 'A';
+
+        while (alphabet <= 'z')
+        {
+                putchar("%c", alphabet);
+                alphabet++;
+	}
+	while (Alphabet <= 'Z'
+		putchar("%c", alphabet);
+		Alphabet++
+        }
+        {
+                putchar("\n");
+        }
+        return (0);
+}                                                           
