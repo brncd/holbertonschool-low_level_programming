3d699ef9d147a2fc2f3c814cd29f683c8347e2e1
brncd <brunocarnales@gmail.com>
2022-09-29 10:37:43 -0700

    2-print_alphabet.c

---
+++ variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+* main- Entry point
+*
+* Return: Always 0 (Success)
+*/
+int main(void)
+{
+	char alphabet = 'a';
+
+	while (alphabet <= 'z')
+	{
+		printf("%c", alphabet);
+		alphabet++;
+	}
+	return (0);
+}
