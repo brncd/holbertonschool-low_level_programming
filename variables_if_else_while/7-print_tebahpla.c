546b28f105e5cf259d8451faf327dbf021fe8e43
brncd <brunocarnales@gmail.com>
2022-10-01 11:07:06 -0700

    7-print_tebahpla.c added

---
+++ variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+* main- Entry point
+*
+* Return: Always 0 (Success)
+*/
+int main(void)
+{
+	char tebahpla = 'z';
+
+	while (alphabet >= 'a')
+	{
+		printf("%c", tebahpla);
+		tebahpla++;
+	}
+	{
+		printf("\n");
+	}
+	return (0);
+}
