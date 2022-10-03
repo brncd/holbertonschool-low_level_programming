f3b8df8e7fd2d8fb610756e7a02b83a9f029d6c2
brncd <brunocarnales@gmail.com>
2022-10-03 09:54:30 -0700

    4-isalpha.c

---
+++ functions_nested_loops/4-isalpha.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+* _islower- Entry point
+*
+* Return: 1, 0
+* @c: int
+*/
+int _isalpha(int c)
+{
+	if (c > 64 && c < 123)
+	{
+		return (1);
+	}
+	else if (c > 96 && c < 123)
+	{
+		
+		return (1);
+	}
+	else 
+	{
+		return (0);
+	}
+}
