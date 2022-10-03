dbedc13776cbe18efd693e3d819aa4d3ee2e608c
brncd <brunocarnales@gmail.com>
2022-10-03 11:16:04 -0700

    7-print_last_digit.c

---
+++ functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+* print_last_digit- Entry point
+*
+* Return: 1, 0
+* @n: int
+*/
+int print_last_digit(int n)
+{
+       int lastDigit = n % 10;
+
+       if (lastDigit >= 0)
+       {
+	       _putchar(lastDigit + '0');
+	       return (lastDigit);
+       }
+       else if (lastDigit < 0)
+       {
+	       lastDigit *= -1;
+	       _putchar(lastDigit + '0');
+       }
+       return (lastDigit);
+}
