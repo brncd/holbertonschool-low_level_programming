4863cf955e4a746bf8a15c60c263e47bf43986db
brncd <brunocarnales@gmail.com>
2022-10-03 17:37:24 -0700

    8-24_hours.c

---
+++ functions_nested_loops/8-24_hours.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+* jack_bauer- Entry point
+*
+*/
+void jack_bauer(void)
+{
+	int minute = 0;
+	int hour = 0;
+
+	while (hour < 24)
+	{
+		for (minute = 0 ; minute < 60 ; minute++)
+		{
+			_putchar((hour / 10) + '0');
+			_putchar((hour % 10) + '0');
+			_putchar(':');
+			_putchar((minute / 10) + '0');
+			_putchar((minute % 10) + '0');
+			_putchar(10);
+			_putchar('\n');
+		}
+		hour++;
+	}
+}
