6c843e008538f3842fe188524d1defe70e0c854c
brncd <brunocarnales@gmail.com>
2022-10-05 13:13:02 -0300

    3-print_numbers.c

---
+++ more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * mul - function
+ * @a: int
+ * @b: int
+ * Return: 0
+ */
+void print_numbers(void)
+{
+	int num;
+
+	for (num = 0; num <=9; num++)
+	{
+		_putchar(num);
+	}
+	_putchar('\b');
+}
