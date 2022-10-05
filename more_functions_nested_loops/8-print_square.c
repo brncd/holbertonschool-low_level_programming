5f50d87a051220c9e8fae603e24a2a0917eb06f0
brncd <brunocarnales@gmail.com>
2022-10-05 15:12:29 -0300

    task 8

---
+++ more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,27 @@
+#include "main.h"
+/**
+ * print_square - function
+ * @size: int
+ */
+void print_square(int size)
+{
+    int count;
+	int num;
+
+	if (n > 0)
+	{
+		for (count = 0; count < size; count++)
+		{
+			for (num = 0; num < count; num++)
+			{
+				_putchar(35);
+			}
+			_putchar(35);
+			_putchar('\n');
+		}
+	}
+	else
+	{
+		_putchar('\n');
+	}
+}
