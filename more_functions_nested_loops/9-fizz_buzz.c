6fec460af71050f81e21c103262131d4480a0dfa
Bruno Carnales <brunocd@tutanota.com>
2022-10-05 20:54:02 -0300

    task 9

---
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+/**
+ *main - function
+ *Return: 0
+ */
+int main(void)
+{
+	int i = 0;
+
+	for (i = 1; i <= 99; i++)
+	{
+		if (i % 15 == 0)
+		{
+			printf("FizzBuzz ");
+		}
+		if (i % 3 == 0 && i % 5 != 0)
+		{
+			printf("Fizz ");
+		}
+		if (i % 5 == 0 && i % 3 != 0)
+		{
+			printf("Buzz ");
+		}
+		if (i % 3 != 0 && i % 5 != 0)
+		{
+			printf("%d ", i);
+		}
+	}
+	printf("Buzz");
+	printf("\n");
+	return (0);
+}
