bf5705ace840b6cb77e7375066867392641c5a11
brncd <brunocarnales@gmail.com>
2022-10-05 18:20:16 -0300

    task 10

---
+++ more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,30 @@
+#include "main.h"
+/**
+ * print_triangle - function
+ * @size: int
+ */
+void print_triangle(int size)
+{
+	int count;
+	int x;
+    int y;
+
+    int = count - x;
+
+	if (size > 0)
+	{
+		for (count = 1; count <= size ; count++)
+		{
+			for (x = 1; x <= y ; x++)
+			{
+				_putchar(' ');
+			}
+			_putchar('#');
+			_putchar('\n');
+		}
+	}
+	else
+	{
+		_putchar('\n');
+	}
+}
