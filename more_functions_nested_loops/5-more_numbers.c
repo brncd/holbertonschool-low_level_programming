3b3977907662d4c604432d07c381bd108f890b8a
brncd <brunocarnales@gmail.com>
2022-10-05 13:56:02 -0300

    task 5

--- more_functions_nested_loops/5-more_numbers.c
+++ more_functions_nested_loops/5-more_numbers.c
@@ -4,16 +4,19 @@
  */
 void more_numbers(void)
 {
-	int count = 0;
+	int coun;
 	int num;
 
-	for (count= 0; count <= 9; count++)
+	for (count = 0; count <= 9; count++)
 	{
-		for (num = 0; num <= 14; num++)
+		for (num = 0; num <= 14; n++)
 		{
-			_putchar(num / 10 + '0');
+			if (num >= 10)
+			{
+				_putchar(num / 10 + '0');
+			}
+			_putchar(num % 10 + '0');
 		}
-		_putchar(num % 10 + '0');
 		_putchar('\n');
 	}
 }
