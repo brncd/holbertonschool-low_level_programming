bbd33f2b7a6d095ced1d81d36110c1a98977e96a
brncd <brunocarnales@gmail.com>
2022-10-05 13:50:19 -0300

    task 5

---
+++ more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * more_numbers - function
+ */
+void more_numbers(void)
+{
+	int count = 0;
+	int num;
+
+	for (count= 0; count <= 9: count++)
+	{
+		for (num = 0; num <= 14; num++)
+		{
+			_putchar(num / 10 + '0');
+		}
+		_putchar(num % 10 + '0');
+		_putchar('\n');
+	}
+}
