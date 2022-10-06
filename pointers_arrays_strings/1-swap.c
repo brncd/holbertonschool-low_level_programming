dc078452b4daca8fb8197f35b6092221f98ea34c
brncd <you@example.com>
2022-10-06 16:19:07 -0700

    betty fixed

--- pointers_arrays_strings/1-swap.c
+++ pointers_arrays_strings/1-swap.c
@@ -1,9 +1,8 @@
 #include "main.h"
 /**
  * swap_int - function
- * @a - int
- * @b - int
- * @c - int
+ * @a: int
+ * @b: int
  */
 void swap_int(int *a, int *b)
 {
--- pointers_arrays_strings/2-strlen.c
+++ pointers_arrays_strings/2-strlen.c
@@ -1,16 +1,16 @@
 #include "main.h"
 /**
  * _strlen - function
- * @s - int
- * @count - int
+ * @s: int
+ * Return: count
  */
 int _strlen(char *s)
 {
 	int count = 0;
-	
-	while (s [count] != '\0')
+
+	while (s[count] != '\0')
 	{
 		count++;
 	}
-	return(count);
+	return (count);
 }
