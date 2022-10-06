1a77329911fdf255b7a63161a98084e13b140c5a
brncd <you@example.com>
2022-10-06 11:31:18 -0700

    2-strlen

---
+++ pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+* _strlen - function
+* 
+*/
+int _strlen(char *s)
+{
+    int count = 0;
+    
+    while (s [count] != '\0')
+    {
+        count++;
+    }
+    return(count);
+}
