0daa40d772a333827279153abc5725a13ead9e36
brncd <you@example.com>
2022-10-07 09:04:40 -0700

    3-puts

--- pointers_arrays_strings/3-puts.c
+++ pointers_arrays_strings/3-puts.c
@@ -1,15 +1,15 @@
 #include "main.h"
 /**
  * _puts - function
- * str: int
+ * @str: int
  */
 void _puts(char *str)
 {
-  int count;
+	int count;
 
-  for( count = 0; str[count] != '\0'; count ++)
-  {
-    _putchar (str[count]);
-  }
-  _putchar (10);
+	for( count = 0; str[count] != '\0'; count ++)
+	{
+		_putchar (str[count]);
+	}
+	_putchar (10);
 }
