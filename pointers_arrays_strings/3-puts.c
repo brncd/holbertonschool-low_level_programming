7f2995c5395e33633182a1c2487df507096b8ccf
brncd <you@example.com>
2022-10-07 06:26:27 -0700

    3-puts.c

---
+++ pointers_arrays_strings/3-puts.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * _puts - function
+ * : int
+ * Return: count
+ */
+void _puts(char *str)
+{
+  int count;
+
+  for(count = 0; str[count] != '\0'; count ++)
+  {
+    _putchar(str[count]);
+  }
+}
