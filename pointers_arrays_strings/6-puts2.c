20333ede8ed668ad13eb6b86feaa652b441fa094
root <brunocarnales@gmail.com>
2022-10-11 06:16:41 -0700

    6-puts2.c

--- pointers_arrays_strings/6-puts2.c
+++ pointers_arrays_strings/6-puts2.c
@@ -1,4 +1,18 @@
+#include "main.h"
+/**
+ * puts2 - function
+ * @str: cnar
+ */
 void puts2(char *str)
 {
-	
+	int count = 0;
+
+	for (count = 0; str[count] != '\0'; count++)
+	{
+		if (count % 2 == 0)
+		{
+			_putchar (str[count]);
+		}
+	}
+	_putchar(count);
 }
