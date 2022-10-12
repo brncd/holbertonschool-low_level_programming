77e7190a6b0308275752fb0a9de2e8abe147e2c3
root <brunocarnales@gmail.com>
2022-10-11 20:02:28 -0700

    8-print_array.c

--- pointers_arrays_strings/8-print_array.c
+++ pointers_arrays_strings/8-print_array.c
@@ -1,16 +1,24 @@
 #include "main.h"
 #include <stdio.h>
+/**
+ * print_array - function
+ * @a: int
+ * @n: int
+ */
 void print_array(int *a, int n)
 {
 	int count;
 
 	for (count = 0; count < n; count++)
 	{
-		if (count < n - 1)
-		{	
+		if (count != n - 1)
+		{
 			printf("%d, ", a[count]);
 		}
+		else
+		{
+			printf("%d", a[count]);
+		}
 	}
-	printf("%d", a[count]); 
-	printf ("\n");
+	printf("\n");
 }
