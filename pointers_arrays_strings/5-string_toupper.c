6f4dfb3e2a899fecbb34993e40e27c4157ad1f68
root <brunocarnales@gmail.com>
2022-10-13 11:25:41 -0700

    5-string_toupper

---
+++ pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ *
+ */
+char *string_toupper(char *str)
+{
+	int i;
+    for (i = 0; str[i] != '\0'; i++)
+    {
+            if (str[i] >= 'a' && str[i] <= 'z')
+                    str[i] =(int)str[i] - 32;
+    }
+    return (str);
+}
