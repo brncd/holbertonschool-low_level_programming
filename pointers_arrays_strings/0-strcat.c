900a395112e59a5f95b4c8dbe0835f83f7c4cb95
root <brunocarnales@gmail.com>
2022-10-12 17:14:30 -0700

    0-strcat

---
+++ pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,18 @@
+#include "main.h"
+char *_strcat(char *dest, char *src)
+{
+	int i=0;
+	int x=0;
+
+	while ( dest[i] != '\0')
+	{
+		i++;
+	}
+	while (src[x] != '\0')
+	{
+		dest[i] = src[x];
+		x++;
+		i++;
+	}	
+return (dest);
+}
