82588254e3a293764e0c0aa36a350fdc15ffe33a
root <brunocarnales@gmail.com>
2022-10-13 13:02:59 -0700

    6-cap_string.c

---
+++ pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,28 @@
+#include "main.h"
+/*
+ *
+ */
+char *cap_string(char *str)
+{
+	int i;
+
+for(i=0; str[i]!='\0'; i++)
+	{
+		if(i==0)
+		{
+			if((str[i]>='a' && str[i]<='z'))
+				str[i]=str[i]-32;
+			continue;
+		}
+		if ((str[i]==' ') || (str[i]=='.') || (str[i]==';') || (str[i]==',') || (str[i]=='!') || (str[i]=='?') || (str[i]=='"') || (str[i]=='(') || (str[i]==')') || (str[i]=='{') || (str[i]=='}'))
+		{
+			++i;
+			if(str[i]>='a' && str[i]<='z')
+			{
+				str[i]=str[i]-32;
+				continue;
+			}
+		}
+	}
+return(str);
+}
