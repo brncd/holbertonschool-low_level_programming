29796cbd0a07ad60ebc622d8ccbd9bf53b0ae909
root <brunocarnales@gmail.com>
2022-10-13 19:48:06 -0700

    1-memcpy.c

---
+++ pointers_arrays_strings/0-memset.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _memset - function 
+ * @s: char 
+ * @b: char
+ * @n: int
+ * Return: n
+ */
+
+char *_memset(char *s, char b, unsigned int n)
+{
+	unsigned int i;
+
+	for (i = 0; i < n ; i++)
+	{
+		s[i] = b;
+	}
+	return (s);
+}
