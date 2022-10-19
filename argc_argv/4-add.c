6557d3b9e1d9586e0ed390bfcdc1a254b8739781
root <brunocarnales@gmail.com>
2022-10-18 19:25:44 -0700

    4-add.c

--- argc_argv/4-add.c
+++ argc_argv/4-add.c
@@ -2,35 +2,24 @@
 #include <stdlib.h>
 #include <ctype.h>
 /**
- * main - function
- * @argc: nt
- * @argv: array
- * Description: prints numberz
- * Return: value 0 (success) *
+ *
  */
 int main(int argc, char *argv[])
 {
-	int i, j, sum = 0;
+	int i, sum = 0;
 	if (argc < 1)
 	{
 		return (0);
 	}
 	for (i = 1; i < argc; i++)
 	{
-		for (j = 0; argv[i][j]; j++)
-		for (j = 1; *(argv[i] + j) != '\0'; j++)
-		{	
-			if ( (isdigit(argv[i][j]) == 0)  )
-			if (*(argv[i] + j) >= 'A' && (*(argv[i] + j) <= 'z'))
-			{
-				printf("Error\n");
-				return (1);
-		}
-		else 
+		if (*argv[i] >= 'A' && *argv[i] <= 'z' )
 		{
-			sum += atoi(argv[i]);
+			printf("%s\n", "Error");
+			printf("Error\n");
+			return (1);
 		}
-		}
+		sum += atoi(argv[i]);
 	}
 	printf("%d\n", sum);
 	return (0);
