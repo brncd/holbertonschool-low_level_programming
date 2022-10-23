84f54e4ca29384f34ec1e0eebf875ca958ac2f98
root <brunocarnales@gmail.com>
2022-10-22 19:39:49 -0700

    0-create_array.c

--- malloc_free/0-create_array.c
+++ malloc_free/0-create_array.c
@@ -12,14 +12,16 @@ char *create_array(unsigned int size, char c)
 	unsigned int i;
 	char *a = NULL;
 
+	a = (char *)malloc(sizeof(char) * size);
 	if (size != 0)
 	{
-		a = (char *)malloc(sizeof(char) * size);
 		for (i = 0; i <= size; i++)
 		{
 			a[i] = c;
 		}
 		return (a);
 	}
+	free (a); 
+	a = NULL;
 	return (NULL);
 }
