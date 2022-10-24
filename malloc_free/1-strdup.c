726fa2249b71ea73c9194c173f4bb3f0ddb1871a
root <brunocarnales@gmail.com>
2022-10-23 19:23:04 -0700

    1-strdup.c

---
+++ malloc_free/1-strdup.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+/**
+ *
+ */
+char *_strdup(char *str)
+{
+	char *a = NULL;
+	int i;
+
+	if (str != NULL)
+	{
+		a = (char *)malloc(sizeof(str));
+		for (i = 0; str[i] != '\0'; i++)
+		{
+			a[i] = str[i];
+		}
+		return (a);
+	}
+	free(a);
+	a = NULL;
+	return (NULL);
+}
