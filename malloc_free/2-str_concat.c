74ee44444189ddf39ec8b9b3eb688373c3979ee3
root <brunocarnales@gmail.com>
2022-10-24 12:02:14 -0700

    2-str_concat.c

--- malloc_free/2-str_concat.c
+++ malloc_free/2-str_concat.c
@@ -13,6 +13,17 @@ char *str_concat(char *s1, char *s2)
 	char *a = NULL;
 	unsigned int i, x;
 
+	if (s1 == NULL || s2 == NULL)
+	{
+		if (s1 == NULL)
+		{
+			s1 = "";
+		}
+		else
+		{
+			s2 = "";
+		}
+	}
 	a = (char *)malloc(sizeof(char) * (strlen(s1) + strlen(s2)) + 1);
 	for (i = 0; i <= strlen(s1) + 1; i++)
 	{
