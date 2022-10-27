1bc6ad176a0472b1f5e15d90c87b5e4dff33db92
root <brunocarnales@gmail.com>
2022-10-26 18:13:20 -0700

    1-string_nconcat.c

--- more_malloc_free/1-string_nconcat.c
+++ more_malloc_free/1-string_nconcat.c
@@ -23,7 +23,7 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	{
 		return (NULL);
 	}
-	for (i = 0; i <= strlen(s1) + 1; i++)
+	for (i = 0; i <= strlen(s1); i++)
 	{
 		p[i] = s1[i];
 	}
