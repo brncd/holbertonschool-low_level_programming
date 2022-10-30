9c19126ea6fe6a376ab8fb93a3c63565591eaf33
root <brunocarnales@gmail.com>
2022-10-30 16:15:59 -0700

    malloc fix

--- structures_typedef/4-new_dog.c
+++ structures_typedef/4-new_dog.c
@@ -2,7 +2,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-/**
+/** 
  * new_dog - creates a new dog
  * @name: name of the dog
  * @age: age of the dog
@@ -20,6 +20,7 @@ dog_t *new_dog(char *name, float age, char *owner)
 	ndog = malloc(sizeof(dog_t));
 	if (ndog == NULL)
 	{
+		free(ndog);
 		return (NULL);
 	}
 	ndog->name = malloc(sizeof(char) * (strlen(name) + 1));
