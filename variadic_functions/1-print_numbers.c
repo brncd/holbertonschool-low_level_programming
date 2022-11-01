1c726e1ba04842581f828bc6eb1fe93770c11058
root <brunocarnales@gmail.com>
2022-10-31 21:42:34 -0700

    task 1 fix

--- variadic_functions/1-print_numbers.c
+++ variadic_functions/1-print_numbers.c
@@ -17,7 +17,7 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 	for (i = 0; i < n; i++)
 	{
 		printf("%d", va_arg(vlist, const unsigned int));
-		if (i != 0)
+		if (i != (n - 1))
 		{
 			printf("%s", separator);
 		}
