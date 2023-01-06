aad616af373121b23875834ad26fdbcbb63d47c8
root <brunocarnales@gmail.com>
2023-01-06 06:50:47 -0800

    task 0 added

--- hash_tables/2-key_index.c
+++ hash_tables/2-key_index.c
@@ -1,6 +1,7 @@
 #include "hash_tables.h"
 /**
  * key_index -gives you the index of a key
+ * @key: is the key
  * @size: is the size of the array of the hash table
  * Returns: the index at which the key/value pair stored in the hash table
  */
